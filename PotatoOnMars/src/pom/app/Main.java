f1e1e0c5fc32aa929b805cbc330f712d3f2d2ce2
Karuskrokro <Karuskrokro@Krokette>
2015-10-22 15:58:51 +0200

    azeaze

--- PotatoOnMars/src/pom/app/Main.java
+++ PotatoOnMars/src/pom/app/Main.java
@@ -1,10 +1,84 @@
 package pom.app;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Set;
+
+import net.seninp.jmotif.sax.NumerosityReductionStrategy;
+import net.seninp.jmotif.sax.SAXException;
+import net.seninp.jmotif.sax.SAXProcessor;
+import net.seninp.jmotif.sax.TSProcessor;
+import net.seninp.jmotif.sax.alphabet.NormalAlphabet;
+import net.seninp.jmotif.sax.datastructure.SAXRecord;
+import net.seninp.jmotif.sax.datastructure.SAXRecords;
+
 public class Main {
+	
+	private final static String dataFName = "D:\\Karus's world\\Cours\\3A\\+ Projet 3A\\Donnees\\48 donnees.txt";
+	private final static int slidingWindowSize = 48;
+	private final static int paaSize = 16;
+	private final static int alphabetSize = 4;
+	private final static NumerosityReductionStrategy nrStrategy = NumerosityReductionStrategy.fromValue(0);
+	private final static double nThreshold = 0.01;
+	
+	private static ArrayList< ArrayList<String> > ranks = new ArrayList< ArrayList<String> >();
 
-	public static void main(String[] args) {
-		//test kappa 123
+	
+	public static void main(String[] args) throws IOException, SAXException {
+		
+		// instantiate classes
+		NormalAlphabet na = new NormalAlphabet();
+		SAXProcessor sp = new SAXProcessor();
 
+		// read the input file
+		double[] ts = TSProcessor.readFileColumn(dataFName, 0, 0);
+
+		// perform the discretization ( 240 motifs != pour chaque fenetre de 24 points)
+		SAXRecords res = sp.ts2saxViaWindow(ts, slidingWindowSize, paaSize, 
+		    na.getCuts(alphabetSize), nrStrategy, nThreshold);
+
+		// print the output
+		Set<Integer> index = res.getIndexes();
+		System.out.println(index.size());
+		for (Integer idx : index) {
+		    System.out.println(idx + ", " + String.valueOf(res.getByIndex(idx).getPayload()));
+		}
+
+		
+		System.out.println("\n------------------------------------------------\n");
+		
+		// Motifs récurrents
+		
+		// get the list of 10 most frequent SAX words
+		ArrayList<SAXRecord> motifs = res.getMotifs(10);
+
+
+		// print best 10 motifs
+		for ( int i = 1; i <= 10; i++ )
+		{
+			SAXRecord topMotif = motifs.get(i-1);
+			System.out.println("top " + i + " motif "+ String.valueOf(topMotif.getPayload()) + " seen " + 
+			            topMotif.getIndexes().size() + " times.");
+		}
+		
+		System.out.println("\n------------------------------------------------\n");
+		
+		// rangement des données
+		for(int i=0; i < slidingWindowSize ; i++)
+		{
+			ranks.add(new ArrayList<String>());
+		}
+		
+		System.out.println(ranks);
+
+		for (Integer idx : index) {
+			//System.out.println(idx);
+			//System.out.println("Je remplis le tableau " + idx%slidingWindowSize);
+			ranks.get(idx%slidingWindowSize).add(String.valueOf(res.getByIndex(idx).getPayload()));
+		}
+		
+    	System.out.println(ranks);
+    	
 	}
 
 }
---
+++ PotatoOnMars/src/pom/app/RandomPatternGenerator.java
@@ -0,0 +1,69 @@
+package pom.app;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class RandomPatternGenerator {
+	private final static int _PATTERN_SIZE_ = 24;
+	private final static int _PATTERN_NUMBER_ = 10;
+	
+	private static File _FILE_=new File("C:\\Users\\Karuskrokro\\Desktop\\generation.txt");
+	private ArrayList<Double> array = new ArrayList<Double>();
+	
+	
+	public void mainPattern(int width) {
+		for(int i = 0 ; i < width-1 ; i++)
+		{
+			this.array.add(Math.random()*2);
+		}
+		this.array.add(Math.random()+5); // Ajout du pic
+	}
+	
+	public void subPatterns(int ammount)
+	{
+		int size = this.array.size();
+		for (int i = 0; i < ammount ; i++)
+		{
+			for(int j = 0 ; j < size ; j++)
+			{
+				this.array.add(this.array.get(j) + Math.random()/3); // Petites diff entres les semaines
+			}
+		}
+	}
+	
+	public void noise(int nbPatterns){
+		int target = (int)(1+ nbPatterns*Math.random());
+		System.out.println("Semaine compliquée : " + target);
+		for( int i = 1; i < _PATTERN_SIZE_ - 2 ; i++) // +1 / - 1 -> Les pics ne sont pas affectés
+		{
+			array.set(target * _PATTERN_SIZE_ + i, array.get(target * _PATTERN_SIZE_ + i) + Math.random()+1); // ajout du rdm
+		}
+	}
+	
+	public void write(ArrayList<Double> array) throws IOException{
+		FileWriter fw = new FileWriter(_FILE_);
+		for (int i = 0 ; i < this.array.size() ; i++)
+		{
+			fw.write(Double.toString(this.array.get(i)));  
+			fw.write("\n"); 
+		}
+		fw.close();
+	}
+	
+	public static void main(String[] args) {
+		RandomPatternGenerator g = new RandomPatternGenerator();
+		try {
+			g.mainPattern(_PATTERN_SIZE_);
+			g.subPatterns(_PATTERN_NUMBER_);
+			g.noise(_PATTERN_NUMBER_);
+			g.write(g.array);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
