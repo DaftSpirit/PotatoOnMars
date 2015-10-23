7092d755481f0cb8df9cb2dec1978bc7d719e2f6
Daft_Spirit <joris.rat@gmail.com>
2015-10-23 11:54:40 +0200

    paasize de 24 et alphabet de 8 lettres

--- PotatoOnMars/src/pom/app/SaxParameters.java
+++ PotatoOnMars/src/pom/app/SaxParameters.java
@@ -5,9 +5,9 @@ import net.seninp.jmotif.sax.NumerosityReductionStrategy;
 public class SaxParameters {
 	
 	final static String dataFName = "data/48 donnees.txt";
-	final static int slidingWindowSize = 23;
-	final static int paaSize = 8;
-	final static int alphabetSize = 4;
+	final static int slidingWindowSize = 24;
+	final static int paaSize = 24;
+	final static int alphabetSize = 8;
 	final static int steps = 24;
 	final static NumerosityReductionStrategy nrStrategy = NumerosityReductionStrategy.fromValue(0);
 	final static double nThreshold = 0.01;
