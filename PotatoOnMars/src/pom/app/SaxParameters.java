9245139f7f4b7d887bccefc5d34dc9aa705c42dc
Karuskrokro <Karuskrokro@Krokette>
2015-10-23 14:35:31 +0200

    azeaze

--- PotatoOnMars/src/pom/app/SaxParameters.java
+++ PotatoOnMars/src/pom/app/SaxParameters.java
@@ -7,7 +7,7 @@ public class SaxParameters {
 	final static String dataFName = "data/48 donnees.txt";
 	final static int slidingWindowSize = 24;
 	final static int paaSize = 24;
-	final static int alphabetSize = 8;
+	final static int alphabetSize = 12;
 	final static int steps = 24;
 	final static NumerosityReductionStrategy nrStrategy = NumerosityReductionStrategy.fromValue(0);
 	final static double nThreshold = 0.01;
