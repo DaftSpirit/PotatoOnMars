1d738d18bca8d30572a93fcdfaa5bdab44f06753
Karuskrokro <Karuskrokro@Krokette>
2015-11-06 09:57:26 +0100

    random functions + main

--- PotatoOnMars/src/pom/app/Analyser.java
+++ PotatoOnMars/src/pom/app/Analyser.java
@@ -1,5 +1,7 @@
 package pom.app;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Set;
 
@@ -57,6 +59,39 @@ public class Analyser {
 			}
 		}		
 		return res;
+	}
+	
+	public double[] medianStraightener5Points(double[] data)
+	{
+		double [] res = new double[data.length];
+		res[0] = (data[0]+data[1]+data[2])/3;
+		res[1] = (data[0]+data[1]+data[2]+data[3])/4;
+		for(int i=2; i < data.length-2; i++)
+		{
+			res[i] = ( data[i-2] + data[i-1] + data[i] + data[i+1] + data[i+2] ) / 5;
+		}
+		res[data.length-2] = (data[data.length-1]+data[data.length-2]+data[data.length-3]+data[data.length-4])/4;
+		res[data.length-1] = (data[data.length-1]+data[data.length-2]+data[data.length-3])/3;
+		return res;
+	}
+	
+	public void dataWritter(double[] data)
+	{
+		try
+		{
+		    FileWriter fw = new FileWriter ("data/48 donnees lissses.txt");
+		 
+		    for (double d : data)
+		    {
+		        fw.write (String.valueOf (d));
+		        fw.write ("\n");
+		    }
+		    fw.close();
+		}
+		catch (IOException exception)
+		{
+		    System.out.println ("Erreur lors de la lecture : " + exception.getMessage());
+		}
 	}
 	
 }
--- PotatoOnMars/src/pom/app/Main.java
+++ PotatoOnMars/src/pom/app/Main.java
@@ -1,12 +1,6 @@
 package pom.app;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Set;
 
 import net.seninp.jmotif.sax.SAXException;
@@ -35,12 +29,6 @@ public class Main {
 
 		// print the output
 		Set<Integer> index = res.getIndexes();
-//		System.out.println(index.size());
-//		for (Integer idx : index) {
-//		    System.out.println(idx + ", " + String.valueOf(res.getByIndex(idx).getPayload()));
-//		}
-//		System.out.println("\n------------------------------------------------\n");
-		
 		
 		// 10 motifs les plus recurrents
 		anal.printRecurrentPatterns(res, 10);
@@ -48,64 +36,22 @@ public class Main {
 		System.out.println("\n------------------------------------------------\n");
     	
 		// Rangement dans le tableau
-
 		PatternList pl = anal.makeRanksByHour(res, index); 
 		
 		//patterns convertis en doubles
 		DoublePatternList converted = anal.convertRanks(pl);
-		//System.out.println(converted);
 		
 		System.out.println("\n------------------------------------------------\n");
 			
 		// LISSAGE
-		
-		double[] ts_lisse = new double[ts.length];
-		ts_lisse[0] = ts[0];
-		ts_lisse[ts.length-1] = ts[ts.length-1];
-		for(int i=1; i < ts.length-1; i++)
-		{
-			ts_lisse[i] = ( ts[i-1] + ts[i] + ts[i+1] ) / 3;
-		}
+		double[] ts_lisse = anal.medianStraightener5Points(ts);
 		
 		SAXRecords res_lisse = sp.ts2saxViaWindow(ts_lisse, SaxParameters.slidingWindowSize, SaxParameters.paaSize, 
 			    na.getCuts(SaxParameters.alphabetSize), SaxParameters.nrStrategy, SaxParameters.nThreshold);
 		Set<Integer> index_lisse = res.getIndexes();
 		
 		PatternList pl_lisse = anal.makeRanksByHour(res_lisse, index_lisse); 
-		try
-		{
-		    FileWriter fw = new FileWriter ("data/48 donnees lissses.txt");
-		 
-		    for (double d : ts_lisse)
-		    {
-		        fw.write (String.valueOf (d));
-		        fw.write ("\n");
-		    }
-		    fw.close();
-		}
-		catch (IOException exception)
-		{
-		    System.out.println ("Erreur lors de la lecture : " + exception.getMessage());
-		}
-    	
-//    	
-//    	//Deprecated : doesn't work
-//    	DataOracle oracle = new DataOracle(ts);
-//    	double future = oracle.prediction();
-//    	System.out.println(future + "the prediction");
-//    	
-//    	System.out.println("\n------------------------------------------------\n");
-//    	
-//    	System.out.println(ts[ts.length-1] + "the real double");
-//    	
-//    	System.out.println("\n-------------------another method for prediction----------------------------\n");
-//    	
-//    	future = oracle.betterPrediction(0.03);
-//    	System.out.println(future + "the prediction");
-//    	
-//    	System.out.println("\n------------------------------------------------\n");
-//    	
-//    	System.out.println(ts[ts.length-1] + "the real double");
+		
     	
 	}
 
