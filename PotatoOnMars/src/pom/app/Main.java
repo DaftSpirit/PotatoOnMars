9fc62087a28a4cde9cfc03c122e0dabc3b9732f1
Daft_Spirit <joris.rat@gmail.com>
2016-01-27 11:15:11 +0100

    #MENAGE

--- PotatoOnMars/src/pom/app/Main.java
+++ PotatoOnMars/src/pom/app/Main.java
@@ -1,10 +1,9 @@
 package pom.app;
 
 import java.io.IOException;
-import java.util.Set;
-
 import pom.core.Analyser;
 import pom.core.LearningTest;
+import pom.core.SAXAnalyser;
 import pom.core.Sorter;
 import pom.util.PatternList;
 import pom.util.SaxParameters;
@@ -12,77 +11,69 @@ import net.seninp.jmotif.sax.SAXException;
 import net.seninp.jmotif.sax.SAXProcessor;
 import net.seninp.jmotif.sax.TSProcessor;
 import net.seninp.jmotif.sax.alphabet.NormalAlphabet;
-import net.seninp.jmotif.sax.datastructure.SAXRecords;
 
 public class Main {
-	
-	
-	public static void main(String[] args) throws IOException, SAXException {
-		
+
+	public static void main(String[] args) {
+
 		// instantiate classes
-		NormalAlphabet na = new NormalAlphabet();
-		SAXProcessor sp = new SAXProcessor();
 		Analyser anal = new Analyser();
 		Sorter so = new Sorter();
 		LearningTest lt = new LearningTest(so);
-		
-		// read the input file
-		double[] ts = TSProcessor.readFileColumn(SaxParameters.dataFName, 0, 0);
-		double[] corrupted_data = TSProcessor.readFileColumn("data/learning/ROC_pic.txt", 0, 0);
+		SAXAnalyser sax = new SAXAnalyser(anal, new SAXProcessor(),
+				new NormalAlphabet());
 
-		System.out.println("\n------------------discretisation-------------------\n");
-
-		// perform the discretization ( 240 motifs != pour chaque fenetre de 24 points)
-		SAXRecords res = sp.ts2saxViaWindow(ts, SaxParameters.slidingWindowSize, SaxParameters.paaSize, 
-		    na.getCuts(SaxParameters.alphabetSize), SaxParameters.nrStrategy, SaxParameters.nThreshold);
-
-		// print the output
-		Set<Integer> index = res.getIndexes();
-		    	
-		// Rangement dans le tableau (patterns)
-		PatternList pl = anal.makeRanksByHour(res, index); 
-		
-		//patterns convertis en doubles
-		//DoublePatternList converted = anal.convertRanks(pl); BUGGED
-		//System.out.println("\n------------------------------------------------\n");
+		System.out
+				.println("\n------------------discretisation-------------------\n");
+		try {
+			double[] corrupted_data;
+			PatternList pl;
+			corrupted_data = TSProcessor.readFileColumn(
+					"data/learning/ROC_pic.txt", 0, 0);
+			double[] ts = TSProcessor.readFileColumn(
+					SaxParameters.dataFName, 0, 0);
 			
-		// LISSAGE
-		double[] ts_lisse = anal.medianStraightener5Points(ts);
-		anal.dataWritter(ts_lisse,"data/48 donnees lissses.txt");
-		
-		System.out.println("\n------------------discretisation lissée-------------------\n");
+			
+			pl = sax.process(ts);
 
-		// Discrétisation sur la liste lisse
-		SAXRecords res_lisse = sp.ts2saxViaWindow(ts_lisse, SaxParameters.slidingWindowSize, SaxParameters.paaSize, 
-			    na.getCuts(SaxParameters.alphabetSize), SaxParameters.nrStrategy, SaxParameters.nThreshold);
-		Set<Integer> index_lisse = res.getIndexes();
-		
-		// Tableau lisse
-		PatternList pl_lisse = anal.makeRanksByHour(res_lisse, index_lisse); 		
-		
-		System.out.println("\n------------------courbAnalyser-------------------\n");
-		
-		double[][] data = anal.courbAnalyser(ts);
-		
-		System.out.println("\n---------------Learning & Percentages-------------\n");
-		
-		System.out.println("\n avec la patternList de base\n");
-		lt.testForPatternList(pl);
-		
-		System.out.println("\n avec la patternList lisse");
-		lt.testForPatternList(pl_lisse);
+			// LISSAGE
+			double[] ts_lisse = anal.medianStraightener5Points(ts);
+			anal.dataWritter(ts_lisse, "data/48 donnees lissses.txt");
 
+			System.out
+					.println("\n------------------discretisation lissée-------------------\n");
 
-		System.out.println("ANALYSE DE TOUTE LA DATA");
-		anal.analyseAlldata(corrupted_data);
+			// Tableau lisse
+			PatternList pl_lisse;
+			double[] data_lisses = TSProcessor.readFileColumn(
+					"data/48 donnees lissses.txt", 0, 0);
+			pl_lisse = sax.process(data_lisses);
 
-		System.out.println("ANALYSE D'UNE DATA");
-		double error = anal.checkAnomaly(2.50056124562001, 16);
-		System.out.println(error);
-		
-		
+			System.out
+					.println("\n------------------courbAnalyser-------------------\n");
+
+			double[][] data = anal.courbAnalyser(ts);
+
+			System.out
+					.println("\n---------------Learning & Percentages-------------\n");
+
+			System.out.println("\n avec la patternList de base\n");
+			lt.testForPatternList(pl);
+
+			System.out.println("\n avec la patternList lisse");
+			lt.testForPatternList(pl_lisse);
+
+			System.out.println("ANALYSE DE TOUTE LA DATA");
+			anal.analyseAlldata(corrupted_data);
+
+			System.out.println("ANALYSE D'UNE DATA");
+			double error = anal.checkAnomaly(2.50056124562001, 16);
+			System.out.println(error);
+
+		} catch (IOException | SAXException e) {
+			e.printStackTrace();
+		}
+
 	}
-	
-	
 
 }
---
+++ PotatoOnMars/src/pom/core/SAXAnalyser.java
@@ -0,0 +1,41 @@
+package pom.core;
+
+import java.io.IOException;
+import java.util.Set;
+
+import pom.util.PatternList;
+import pom.util.SaxParameters;
+import net.seninp.jmotif.sax.SAXException;
+import net.seninp.jmotif.sax.SAXProcessor;
+import net.seninp.jmotif.sax.TSProcessor;
+import net.seninp.jmotif.sax.alphabet.NormalAlphabet;
+import net.seninp.jmotif.sax.datastructure.SAXRecords;
+
+public class SAXAnalyser {
+	
+	private Analyser anal;
+	private SAXProcessor sp;
+	private NormalAlphabet na;
+	
+	public SAXAnalyser(Analyser anal, SAXProcessor sp, NormalAlphabet na) {
+		this.anal = anal;
+		this.sp = sp;
+		this.na = na;
+	}
+	
+	public PatternList process(double[] datas) throws IOException, SAXException{
+		
+		System.out.println("\n------------------discretisation-------------------\n");
+
+		// perform the discretization ( 240 motifs != pour chaque fenetre de 24 points)
+		SAXRecords res = sp.ts2saxViaWindow(datas, SaxParameters.slidingWindowSize, SaxParameters.paaSize, 
+		    na.getCuts(SaxParameters.alphabetSize), SaxParameters.nrStrategy, SaxParameters.nThreshold);
+
+		// print the output
+		Set<Integer> index = res.getIndexes();
+		
+		PatternList pl = anal.makeRanksByHour(res, index);
+		
+		return pl;
+	}
+}
